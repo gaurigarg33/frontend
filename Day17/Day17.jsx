d553753e238383db9268c15b3b352e2c88a2b47b
Gauri Garg <68108677+gaurigarg33@users.noreply.github.com>
2021-07-11 14:48:31 +0530

    Create Day17.jsx

---
+++ Day17/Day17.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Day17Cards from "./Day17Cards";
+function Day17()
+{
+    return(
+        <>
+        <div className="main2">
+       <Day17Cards food="Pizza" calorie="You have consumed 56 cals today"/>
+       <Day17Cards food="Burger" calorie="You have consumed 69 cals today"/>
+       <Day17Cards food="Coke" calorie="You have consumed 500 cals today"/>
+       <Day17Cards food="Browne" calorie="You have consumed 180 cals today"/>
+       <Day17Cards food="Fried Rice" calorie="You have consumed 90 cals today"/>
+       <Day17Cards food="Lassania" calorie="You have consumed 200 cals today"/>
+       <Day17Cards food="Pani puri" calorie="You have consumed 10 cals today"/>
+       </div>
+      
+        </>
+    );
+
+}
+export default Day17;
