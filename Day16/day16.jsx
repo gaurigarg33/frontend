15ce22676501e3d87b047b615155fbed160de637
Gauri Garg <68108677+gaurigarg33@users.noreply.github.com>
2021-07-11 14:46:24 +0530

    Create day16.jsx

---
+++ Day16/day16.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Day16.css';
+import Card from "./Day16Card";
+function Day16()
+{
+    return(
+        <div>
+           <Card/>
+        </div>
+        
+        );
+}
+export default Day16;
