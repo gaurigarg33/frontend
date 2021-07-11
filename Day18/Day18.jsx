30a4bcd3d5534e5649332189d0a99ae28dd28394
Gauri Garg <68108677+gaurigarg33@users.noreply.github.com>
2021-07-11 14:51:32 +0530

    Create Day18.jsx

---
+++ Day18/Day18.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Day18.css";
+import Day18Board from "./Day18_board";
+function Day18()
+{
+    return(
+        <>
+         
+          <div  className="chess">
+             <Day18Board/>
+             <Day18Board/>
+             <Day18Board/>
+             <Day18Board/>
+             <Day18Board/>
+             <Day18Board/>
+        </div>
+
+        
+        
+        </>
+    );
+}
+export default Day18;
