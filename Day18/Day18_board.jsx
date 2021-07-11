aa7d5ab72095db81d3483e1e0aa4a282546ecba5
Gauri Garg <68108677+gaurigarg33@users.noreply.github.com>
2021-07-11 14:52:22 +0530

    Add files via upload

---
+++ Day18/Day18_board.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Day18.css";
+function Day18Board()
+{
+    return (
+        <>
+        <table>
+            <tr>
+                <td className="black"></td>
+                <td className="white"></td>
+                <td className="black"></td>
+                <td className="white"></td>
+                <td className="black"></td>
+                <td className="white"></td>
+                <td className="black"></td>
+                <td className="white"></td>
+                <td className="black"></td>
+                <td className="white"></td>
+                <td className="black"></td>
+                <td className="white"></td>
+                <td className="black"></td>
+            </tr>
+
+            <tr>
+                <td className="white"></td>
+                <td className="black"></td>
+                <td className="white"></td>
+                <td className="black"></td>
+                <td className="white"></td>
+                <td className="black"></td>
+                <td className="white"></td>
+                <td className="black"></td>
+                <td className="white"></td>
+                <td className="black"></td>
+                <td className="white"></td>
+                <td className="black"></td>
+                <td className="white"></td>
+            </tr>
+
+        </table>
+        </>
+    );
+
+}
+
+export default Day18Board;
