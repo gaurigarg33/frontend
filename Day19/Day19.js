801947f496e3d53b9ef41d5d236281c1d612a216
Gauri Garg <68108677+gaurigarg33@users.noreply.github.com>
2021-07-12 13:51:43 +0530

    Create Day19.js

---
+++ Day19/Day19.js
@@ -0,0 +1,13 @@
+import './Day19.css';
+import Count from "./count";
+function Day19() {
+  return (
+    <div className="counter">
+        <Count></Count>
+        <Count></Count>
+        <Count></Count>
+        <Count></Count>
+      </div>
+  );
+}
+export default Day19;
